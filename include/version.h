1dc0aaefe23518094f96518fde1dc92239cc7720
4ipset <228gus228uu@gmail.com>
2026-08-22 22:43:54 +0400

    fix version.h

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-static inline constexpr const char* QUARK_ENGINE_VERSION = "0.1.0";
+static inline constexpr const char* QUARK_ENGINE_VERSION = "v1.0.0";
 
 #endif // __VERSION_H__
